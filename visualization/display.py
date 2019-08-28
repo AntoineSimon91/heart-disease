80eaa3e4b9adfdac658e1b8ac00babbd00f70906
antoineSimon91 <antoine.simon91@gmail.com>
2019-08-28 22:42:39 +0200

    Display correlation matrix

---
+++ visualization/display.py
@@ -0,0 +1,25 @@
+
+# third-party imports
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+def display_correlation_matrix(df):
+    corr = df.corr()
+    sns.set(style="white")
+
+    # Generate a mask for the upper triangle
+    mask = np.zeros_like(corr, dtype=np.bool)
+    mask[np.triu_indices_from(mask)] = True
+
+    # Set up the matplotlib figure
+    f, ax = plt.subplots(figsize=(11, 9))
+
+    # Generate a custom diverging colormap
+    cmap = sns.diverging_palette(220, 10, as_cmap=True)
+
+    # Draw the heatmap with the mask and correct aspect ratio
+    sns.heatmap(corr, mask=mask, cmap=cmap, vmax=.3, center=0,
+                square=True, linewidths=.5, cbar_kws={"shrink": .5})
+    plt.show()
